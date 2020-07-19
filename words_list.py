d9a09701aabc0962ffccdbb6bda8a38aa30d6a8b
Roger-Aguiar <roger.silvaaguiar@yahoo.com.br>
2020-07-19 11:44:12 -0300

    New words added.

--- words_list.py
+++ words_list.py
@@ -1,15 +1,17 @@
 # Name:         Roger Silva Santos Aguiar
 # Function:     This module contains a list of words for the hangman game.
 # Initial date: July 17, 2020
-# Last update:  July 17, 2020
+# Last update:  July 19, 2020
 
 # Required modules
 import random
 
 class Words:
     def __init__(self):
-        self.words = ["pheasant", "duck", "turkey", "chicken", "vulture", "full",
-                      "albatross", "puffin", "lion", "horn", "beaver", "koala", "kangaroo"]
+        self.words = ['aback', 'abacus', 'abalone', 'abandon', 'abandoned', 'abandonment', 'abase', 'abashed',
+                      'abate', 'abatoir', 'abaya', 'abba', 'abbess', 'abbey', 'abbot', 'abbreviate', 'abbreviation',
+                      'abdicate', 'abdomen', 'abdominal', 'abduct', 'abductee' 'abductor', 'abed', 'aberrant',
+                      'aberration', 'abet', 'abhor', 'abhorrence']
 
     def choose_word(self):
         return random.choice(self.words)
