c1d0b33da5f23e09cf2d3d1962ddcba84e78fdf2
Roger-Aguiar <roger.silvaaguiar@yahoo.com.br>
2020-07-19 10:02:01 -0300

    Method to display you lose was created.

--- hangman_game.py
+++ hangman_game.py
@@ -1,7 +1,7 @@
 # Name:         Roger Silva Santos Aguiar
 # Function:     This is the main module of the application, the hangman game.
 # Initial date: July 17, 2020
-# Last update:  July 18, 2020
+# Last update:  July 19, 2020
 
 # Required modules
 import words_list
@@ -36,12 +36,14 @@ class HangmanGame:
             else:
                 self.error = self.error + 1
                 attempts = attempts - 1
-                hangman_drawings.HangmanDrawing.draw_hangman_drawing(self.error)
-                print("You have {} more attempts.".format(attempts))
-                print("Your word: " + word_of_the_player)
+
+                if attempts > -1:
+                    hangman_drawings.HangmanDrawing.draw_hangman_drawing(self.error)
+                    print("You have {} more attempts.".format(attempts))
+                    print("Your word: " + word_of_the_player)
             if self.error > 6:
                 hangman_drawings.HangmanDrawing.draw_hangman_drawing(self.error)
-                print('You lose!')
+                self.display_you_lose()
                 print('Word of the game: ' + word_of_the_game)
                 break
 
@@ -64,22 +66,41 @@ class HangmanGame:
         return word_of_the_player
 
     def display_you_win(self):
-        winner = """
-                    *          *    ********        *               *    *                                       * * *           *   *
-                     *        *  *            *     *               *     *                                     *    **          *   *
-                      *      * *               *    *               *      *                                   *   * * *         *   *
-                       *    * *                 *   *               *       *                                 *    * *  *        *   *
-                        *  * *                   *  *               *        *               *               *     * *   *       *   *
-                         ** *                     * *               *         *             * *             *      * *    *      *   *
-                         ** *                     * *               *          *           *   *           *       * *     *     *   *
-                         ** *                     * *               *           *         *     *         *        * *      *    *   *
-                         **  *                   *  *               *            *       *       *       *         * *       *   *   *
-                         **   *                 *    *             *              *     *         *     *          * *        *  *   *
-                         **    *               *      *           *                *   *           *   *           * *         * *   *
-                         **     *             *        *         *                  * *             * *            * *          **
-                         **        **********            ********                    *               *             * *           *   *
+        message = """
+                    *           *    ********        *               *    *                                       * * *           *   *
+                     *         *  *            *     *               *     *                                     *    **          *   *
+                      *       * *               *    *               *      *                                   *   * * *         *   *
+                       *     * *                 *   *               *       *                                 *    * *  *        *   *
+                        *   * *                   *  *               *        *               *               *     * *   *       *   *
+                         * * *                     * *               *         *             * *             *      * *    *      *   *
+                          *  *                     * *               *          *           *   *           *       * *     *     *   *
+                          *  *                     * *               *           *         *     *         *        * *      *    *   *
+                          *   *                   *  *               *            *       *       *       *         * *       *   *   *
+                          *    *                 *    *             *              *     *         *     *          * *        *  *   *
+                          *     *               *      *           *                *   *           *   *           * *         * *   *
+                          *      *             *        *         *                  * *             * *            * *          **
+                          *        **********            ********                     *               *             * *           *   *
                  """
-        print(winner)
+        print(message)
+
+    def display_you_lose(self):
+        message = """
+                    *           *    ********        *               *    *                  ********         **********  ***********   * 
+                     *         *  *            *     *               *    *                *          *     *             *             *
+                      *       * *               *    *               *    *               *            *   *              *             *
+                       *     * *                 *   *               *    *              *              *  *              *             *       
+                        *   * *                   *  *               *    *             *                *  *             *             *
+                         * * *                     * *               *    *            *                  *  *******      *             *
+                          *  *                     * *               *    *            *                  *          *    ***********   *
+                          *  *                     * *               *    *            *                  *            *  *             *
+                          *   *                   *  *               *    *             *                *              * *             *
+                          *    *                 *    *             *     *              *              *               * *             *
+                          *     *               *      *           *      *               *            *               *  *             *
+                          *      *             *        *         *       *                *          *               *   *             
+                          *        **********            ********         * ************     ********        **********   ***********   *
+                              
+                  """
+        print(message)
 
 
 if __name__ == '__main__':
